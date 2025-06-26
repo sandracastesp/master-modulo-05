1bd803fcc0d98533d5c1a8802d8e2deb8de05406
Sandra Castro España <sandracaes00@gmail.com>
2025-06-25 23:41:57 -0600

    Se modifican los aspectos esenciales de node y npm a través de formato taller para la configurar un archivo Node.js desde cero, utilización de sistemas de módulos de Node.js y crear scripts en package.json para automatizar tareas

---
+++ practica-siete/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,29 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Neptuno",
+    descripcion: "Octavo planeta del sistema solar, conocido por sus intensas tormentas y color azul profundo.",
+    descubiertoEn: "1846"
+  },
+  {
+    nombre: "Ceres",
+    descripcion: "Es el planetoide más grande del cinturón de asteroides entre Marte y Júpiter, y está clasificado como planeta enano.",
+    descubiertoEn:"1801"
+  },
+  {
+    nombre: "Ganímedes",
+    descripcion: "Luna más grande del sistema solar, con un nucleo rocoso y una capa de hielo",
+    descubiertoEn:"1610"
+  },
+];
+
+module.exports = planetas;
